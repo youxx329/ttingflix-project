72c862ec6ce41331bad24c1ffffb887b564faf3f
youxx329 <younggu329@naver.com>
2025-04-18 14:25:14 +0900

    nav 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import NotFoundPage from './NotFoundPage.jsx/NotFoundPage';
+import Homepage from './pages/Homepage/Homepage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import MoviePage from './pages/Movies/MoviePage';
+
+// 홈페이지 /
+// 영화 전체 보여주는 페이지 (서치 기능) /movies
+// 영화 디테일 페이지 /movies/:id
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            {' '}
+            // user화면
+            <Route index element={<Homepage />} />
+            <Route path="movies">
+              <Route index element={<MoviePage />} />
+              <Route path=":id" element={<MovieDetailPage />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,64 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outlet } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary my-nav-bg">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img
+              src="/public/logo-org.png"
+              alt="ttingflix"
+              height="30"
+              width="100"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0 my-nav-color"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <LinkContainer to="/">
+                <Nav.Link>홈</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/movies">
+                <Nav.Link>영화</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="">
+                <Nav.Link>내가 찜한 리스트</Nav.Link>
+              </LinkContainer>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button className="btn-orange">
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  className="ico-search"
+                />
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppLayout;

7100eecbe741acdfc59e857b9e87481cea50fc39
youxx329 <younggu329@naver.com>
2025-04-20 23:56:12 +0900

    banner 작업

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Banner from './components/Banner/Banner';
 
+// 1. 배너 => popular 영화를 들고와서 첫 번째 아이템을 보여주자
+// 2. popular movie
+// 3. top rated movie
+// 4. upcoming movie
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div>
+      <Banner />
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
